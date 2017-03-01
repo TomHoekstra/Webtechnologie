8844cb13fa652548ab556b70ba3f249591c4b139
Tom Hoekstra <tom-1509@hotmail.com>
2017-03-01 19:10:41 +0100

    wt - practicum2 init

---
+++ Practicum2/README.txt
@@ -0,0 +1,7 @@
+Beste beoordelaar,
+
+De navigatie van deze site is gebouwd met behulp van angular routing. Dit zorgt ervoor dat wij een masterpage hebben die ervoor zorgt dat je de header en footer niet in meerdere pagina's hoeft te stoppen.
+Het enige nadeel hiervan is dat de website gehost moet worden op een website of op een lokale webserver. Mocht dat niet lukken om dat zelf te doen dan is de website hier te bekijken: 
+https://wt-practicum1.firebaseapp.com/#/home
+De website is geschikt gemaakt voor alle devices. Dus schroom niet om hem te bekijken op je telefoon of tablet.
+
---
+++ Practicum2/scripts/app.js
@@ -0,0 +1,22 @@
+angular.module('gylfiApp', ['ngRoute', 'ui.bootstrap']).config(function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'pages/home.html',
+        })
+        .when('/swansea', {
+            templateUrl: 'pages/swansea.html',
+        })
+        .when('/iceland', {
+            templateUrl: 'pages/iceland.html',
+        })
+        .when('/stats', {
+            templateUrl: 'pages/stats.html',
+        })
+        .when('/bestof', {
+            templateUrl: 'pages/bestof.html',
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+});
