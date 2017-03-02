e412ec7a6c72c0143a92eb093590302b00a94489
Tom Hoekstra <tom-1509@hotmail.com>
2017-03-02 21:34:42 +0100

    maploader fix

--- Practicum2/scripts/mapController.js
+++ Practicum2/scripts/mapController.js
@@ -32,43 +32,9 @@ angular.module('gylfiApp').controller('mapController', [function () {
         });
     };
 
-//Keep for generating JSON
-    //     _this.createEuropeanMarkers();
-    // this.markers = [];
-    //     this.createEuropeanMarkers = function () {
-    //         /*Saint Ettiene*/
-    //         this.markers.push(new google.maps.Marker({
-    //             position: { lat: 45.4607318, lng: 4.3901162 },
-    //             label: "G",
-    //             title: "Portugal 1 - 1 Iceland"
-    //         }));
-    //         /*Marseille*/
-    //         this.markers.push(new google.maps.Marker({
-    //             position: { lat: 43.269827, lng: 5.3958873 },
-    //             label: "Y",
-    //             title: "Iceland 1 - 1 Hungary"
-    //         }));
-    //         /*Saint Denis*/
-    //         this.markers.push(new google.maps.Marker({
-    //             position: { lat: 48.936181, lng: 2.457443 },
-    //             label: "L",
-    //             title: "Iceland 2 - 1 Austria"
-    //         }));
-    //         /*Nice*/
-    //         this.markers.push(new google.maps.Marker({
-    //             position: { lat: 43.7050594, lng: 7.192585 },
-    //             label: "F",
-    //             title: "England 1 - 2 Iceland"
-    //         }));
-    //         /*Saint Denis*/
-    //         this.markers.push(new google.maps.Marker({
-    //             position: { lat: 48.936181, lng: 2.357443 },
-    //             label: "I",
-    //             title: "France 5 - 2 Iceland"
-    //         }));
-    //     };
-
     google.maps.event.addDomListener(window, 'load', this.initialize);
+    //Fix for not loading first time
+    $(document).bind("projectLoadComplete", this.initialize);
 }]);
 
 
