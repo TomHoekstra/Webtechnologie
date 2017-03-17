ae1a1871f375df0e80d03cb59cc79c313dd654c4
Tom Hoekstra <tom-1509@hotmail.com>
2017-03-17 22:26:49 +0100

    fixes

--- Practicum2/README.txt
+++ Practicum2/README.txt
@@ -2,4 +2,5 @@ Beste beoordelaar,
 
 De navigatie van deze site is gebouwd met behulp van angular routing. Dit zorgt ervoor dat ik een masterpage heb die ervoor zorgt dat je de header en footer niet in meerdere pagina's hoeft te stoppen.
 Het enige nadeel hiervan is dat de website gehost moet worden op een website of op een lokale webserver. Mocht dat niet lukken om dat zelf te doen dan is de website hier te bekijken: 
-https://wt-practicum2.firebaseapp.com/#/home
+https://wt-practicum2.firebaseapp.com/#/home Dit is afgesproken met Sergey, dat het ook goed zou zijn als hij online rendert en niet lokaal.
+Net als dat ik geen punten aftrek meer zou krijgen voor het gebruik van Bootstrap, omdat ik dit in opdracht 1 al had gekregen.
--- Practicum2/contextmenu/contextmenu.js
+++ Practicum2/contextmenu/contextmenu.js
@@ -5,38 +5,57 @@ $(document).on("contextmenu", "body", function (e) {
         top: event.pageY + "px",
         left: event.pageX + "px"
     });
-
 });
 
 $(document).bind("mousedown", function (e) {
-
     if (!$(e.target).parents(".contextmenu").length == 0) {
         if (e.button == 0) {
             switch ($(e.target)[0].id) {
-                case "normal_font":
+                case "normal_font_site":
                     $("body").css("font-weight", "normal");
                     $("body").css("font-style", "normal");
                     break;
-                case "bold_font":
+                case "bold_font_site":
                     $("body").css("font-weight", "bold");
                     $("body").css("font-style", "normal");
                     break;
-                case "italic_font":
+                case "italic_font_site":
                     $("body").css("font-style", "italic");
                     $("body").css("font-weight", "normal");
                     break;
-                case "normal_colors":
+                case "normal_colors_site":
                     setNormalColor("#02529C", "#DC1E35");
                     break;
-                case "inverted_colors":
+                case "inverted_colors_site":
                     setNormalColor("#DC1E35", "#02529C");
                     alert("Its ugly though!");
+                    break;
+                case "normal_font_element":
+                    $($("#originalElement").data()).css("font-weight", "normal");
+                    $($("#originalElement").data()).css("font-style", "normal");
+                    break;
+                case "bold_font_element":
+                    $($("#originalElement").data()).css("font-weight", "bold");
+                    $($("#originalElement").data()).css("font-style", "normal");
+                    break;
+                case "italic_font_element":
+                    $($("#originalElement").data()).css("font-style", "italic");
+                    $($("#originalElement").data()).css("font-weight", "normal");
+                    break;
+                case "bigger_element":
+                    $($("#originalElement").data()).css("font-size", "74px");
+                    break;
+                case "smaller_element":
+                    $($("#originalElement").data()).css("font-size", "10px");
                     break;
             }
         }
         else {
             return;
         }
+    }
+    else {
+        $("#originalElement").data(e.target);
     }
 
     $(".contextmenu").hide(100);
